734324cc4d84271cafb9c0db318ab926c598636c
Timothy Messier <tim.messier@gmail.com>
2014-01-29 15:09:25 -0500

    Added bundle option to installer

--- install.sh
+++ install.sh
@@ -8,11 +8,13 @@ function print_help() {
 optional args:
 
     -p|--pretend  print what install will do without doing it.
+    -b|--bundle   run :BundleUpdate after install.
     -h|--help     print this help."
 }
 
 pretend=0
-OPTS=$(getopt -o ph --long pretend,help -n "$name" -- "$@")
+bundleupdate=0
+OPTS=$(getopt -o pbh --long pretend,bundle,help -n "$name" -- "$@")
 
 if [ $? != 0 ]; then echo "option error" >&2; exit 1; fi
 
@@ -22,6 +24,9 @@ while true; do
     case "$1" in
         -p|--pretend)
             pretend=1
+            shift;;
+        -b|--bundle)
+            bundleupdate=1
             shift;;
         -h|--help)
             print_help
@@ -56,7 +61,7 @@ for dot in $(ls); do
     fi
 done
 
-# Cleanup 
+# Cleanup
 rm -rf "$HOME/.dzen/dzen"
 
 if [[ $pretend -eq 1 ]]; then
@@ -71,11 +76,22 @@ if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
     else
         echo "Installing vundle"
         git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle" &> /dev/null
-        echo "Now start vim and run:"
-        echo ":BundleInstall"
+        if [[ $bundleupdate -eq 0 ]]; then
+            echo "Now start vim and run:"
+            echo ":BundleInstall"
+        fi
     fi
 else
     echo "Vundle already installed"
+fi
+
+if [[ $bundleupdate -eq 1 ]]; then
+    if [[ $pretend -eq 1 ]]; then
+        echo "Would run :BundleUpdate"
+    else
+        echo "Running bundle update"
+        vim -c BundleUpdate -c qa &> /dev/null
+    fi
 fi
 
 popd &> /dev/null
